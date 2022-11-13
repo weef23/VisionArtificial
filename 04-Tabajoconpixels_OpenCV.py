d70d600792528a3af06fce5e10a5273f9a85a078
Wilmer Estrada <weef23@hotmail.com>
2022-11-13 10:29:36 -0600

    Manejo de Pixeles con OpenCV

--- 04-Tabajoconpixels_OpenCV.py
+++ 04-Tabajoconpixels_OpenCV.py
@@ -1,11 +1,30 @@
 ### Manejo de Pixeles con OpenCV
 import cv2
 import numpy as np
-
+#######################################################################################################################
 ### Creamos una matriz de enteros de 8 bits de 100 * 100 de 3 dimensiones
 ### Usamos una dimension para cada canal RGB
 imagenObscura = np.zeros((100, 100, 3), np.uint8)
-
 #################################################################################
+imagenObscura[97, 97] = [255, 255, 255]
 pixel = imagenObscura[97, 97]
+## Los rangos de valores en un canal RGB van entre 0-255
+## Cada dimension representa un canal cuyo valor va entre 0 y 255
+## A continuacion tenemos el color Blanco.
 print(pixel)
+## Obtener las dimensiones de una imagen generada
+alto, ancho, canales = imagenObscura.shape
+print(alto, ancho,canales)
+
+### Podemos recorrer la imagen pixel por pixel
+for i in range(alto):
+    for j in range(ancho):
+        print(imagenObscura[i, j])
+
+### Modificamos el color de todos los pixeles con excepcion del pixel blanco
+for i in range(alto):
+    for j in range(ancho):
+        pixel[i, j] = imagenObscura[i, j]
+
+        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+            imagenObscura = [0, 0, 255] ## Color azul
