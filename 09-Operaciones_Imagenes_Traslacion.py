eb05101336b65b84900819a14d34a6334f986950
Wilmer Estrada <weef23@hotmail.com>
2022-11-21 22:56:38 -0600

    Actualizacion Operacion Traslacion

--- 09-Operaciones_Imagenes_Traslacion.py
+++ 09-Operaciones_Imagenes_Traslacion.py
@@ -1,4 +1,4 @@
-################### OPERACIONES /TRANSFORMACION DE UNA EMPRESA ##########################
+################### OPERACIONES/TRANSFORMACION DE UNA EMPRESA ##########################
 import numpy as np
 import cv2
 
