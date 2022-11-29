8e90ce478c0a378ccfc4a055e1d0e9a7268eae17
Wilmer Estrada <weef23@hotmail.com>
2022-11-28 18:25:26 -0600

    Operacion de Escalacion de imagenes

--- 11-Operaciones_Imagenes_Escalacion.py
+++ 11-Operaciones_Imagenes_Escalacion.py
@@ -4,5 +4,37 @@ import cv2
 #######################################################################################################################
 ### Traslacion de una imagen usando OpenCV
 img = cv2.imread("./Images/1.jpg")
-imagen2 = cv2.resize(img, (200, 200)) ## Redimensionamos la imagen a 100 x 50
-alto, ancho, canal = imagen2.shape
+alto, ancho, canal = img.shape
+## Definimos la escala que ha de tomar la imagen
+escala = 2
+## Definimos la matriz de escalacion
+matrizEscalacion = np.array([[escala, 0, 0], [0, escala, 0], [0, 0, 1]])
+## Definimos una imagen vacia
+imagenEscalada = np.zeros((alto * escala, ancho*escala, 3), np.uint8)
+#######################################################################################################################
+for i in range(alto):
+    for j in range(ancho):
+        ## Vector pixel, este vector contiene cada uno de los pixeles de la imagen
+        pixel = img[i, j]
+        ## Define el vector de la posicion de la imagen
+        vectorPosicion = [j, i, 1]
+        dot = np.dot(matrizEscalacion, vectorPosicion)
+        x = dot[0]
+        y = dot[1]
+        ## Guardamos los pixeles en la posicion resultante de la imagen escalada
+        imagenEscalada[y, x] = pixel
+########################################################################################################################
+## OpenCV tiene la funcion resize para realizar esta misma operacion de forma mas eficiente, ya la hemos usado antes
+imagenEscalada2 = cv2.resize(img, (1024, 720), interpolation=cv2.INTER_LINEAR)
+########################################################################################################################
+### Mostramos la imagen original y la imagen escalada
+cv2.namedWindow("original", cv2.WINDOW_NORMAL)
+cv2.namedWindow("escalada", cv2.WINDOW_NORMAL)
+cv2.namedWindow("escalada2", cv2.WINDOW_NORMAL)
+########################################################################################################################
+cv2.imshow("original", img)
+cv2.imshow("escalada", imagenEscalada)
+cv2.imshow("escalada2", imagenEscalada2)
+########################################################################################################################
+cv2.waitKey()
+cv2.destroyAllWindows()
