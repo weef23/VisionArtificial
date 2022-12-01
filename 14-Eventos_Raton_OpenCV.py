6ed78cda23a43a07cccc7e170efb2dc46aeff81a
Wilmer Estrada <weef23@hotmail.com>
2022-11-30 18:28:16 -0600

    Eventos del Raton

--- 14-Eventos_Raton_OpenCV.py
+++ 14-Eventos_Raton_OpenCV.py
@@ -2,16 +2,24 @@
 import cv2
 import numpy as np
 ### Funcion para dibujar circunferencia
-def dibujar_Circulo(event,x,y,flags,param):
-    print(event)
-    if(event == cv2.EVENT_LBUTTONDOWN):
-        cv2.circle(img, (x, y), 100, (255, 255, 0), -1)
+def eventosRaton(event,x,y,flags,param):
+    if(event == 1):
+        print(f"Clic Izquierdo X => {x}, Y=> {y}")
+    elif (event == 2):
+        print(f"Clic Derecho X => {x}, Y=> {y}")
+    elif (event == 3):
+        print(f"Clic boton central X => {x}, Y=> {y}")
+    elif (event == 4):
+        print(f"Se suelta el boton izquierdo X => {x}, Y=> {y}")
+    elif (event == 5):
+        print(f"Se suelta el boton derecho X => {x}, Y=> {y}")
+
 #######################################################################################################################
 ## Creamos una imagen vacia
 img =  np.zeros((512, 512, 3), np.uint8)
-cv2.namedWindow("ImagenVacia", cv2.WINDOW_NORMAL)
+cv2.namedWindow("ImagenVacia")
 
-cv2.setMouseCallback("ImagenVacia", dibujar_Circulo)
+cv2.setMouseCallback("ImagenVacia", eventosRaton)
 ########################################################################################################################
 while True:
     cv2.imshow("ImagenVacia", img)
