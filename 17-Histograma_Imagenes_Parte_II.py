2383066d90050e4b457b2628a5eb700b77372a2e
Wilmer Estrada <weef23@hotmail.com>
2022-12-03 22:14:50 -0600

    Histograma de una imagen

--- 17-Histograma_Imagenes_Parte_II.py
+++ 17-Histograma_Imagenes_Parte_II.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as ptl
 ########################################################################################################################
@@ -21,3 +22,18 @@ for i, col in enumerate(color):
     hist = cv2.calcHist([img], [i], None, [256], [0, 255])
     ptl.plot(hist, color=col)
     ptl.xlim([0, 256]) ## Este es el rango de valores de los colores de la imagen.
+########################################################################################################################
+#### Uso de mascara para el calculo de histogramas
+mascara = np.zeros(img.shape[:2], np.uint8)
+mascara[0:300, 0:300] = 255
+### Aplicamos una compuerta and bit a bir entre la misma y imagen pero unicamente a los pixeles blancos
+masked_img = cv2.bitwise_and(I, I, mask=mascara)
+hist_Completo = cv2.calcHist([I], [0], None, [256], [0, 255])
+masked_img_hist = cv2.calcHist([masked_img], [0], None, [256], [0, 255])
+########################################################################################################################
+plt.subplot(221), plt.imshow(I,"gray")
+plt.subplot(222), plt.imshow(mascara,"gray")
+plt.subplot(223), plt.imshow(masked_img, "gray")
+plt.subplot(224), plt.plot(hist_Completo), plt.plot(masked_img_hist)
+plt.xlim([0, 255])
+plt.show()
