6701c7374c63d6f05b67e44fa3c9733618adc818
Wilmer Estrada <weef23@hotmail.com>
2022-12-07 20:22:17 -0600

    Ecualizacion de Histogramas

---
+++ 18-Ecualizacion_Imagenes_OpenCV.py
@@ -0,0 +1,7 @@
+##### Ecualizacion del Histograma
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as ptl
+########################################################################################################################
+img = cv2.imread("Images/2.jpg")
